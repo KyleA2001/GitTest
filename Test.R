177b646edb2645ad882b8b50ac21308e64cdd4ea
Kyle Allie <kyledrallie@gmail.com>
2025-02-13 09:35:42 -0400

    More testing

---
+++ Test.R
@@ -0,0 +1,16 @@
+# Nothing to do here, just a test
+
+
+#More testing
+
+
+# Small edit to test Git push
+
+
+# Random Code to Test Git Push
+random_numbers <- sample(1:100, 10)  # Generate 10 random numbers between 1 and 100
+mean_random <- mean(random_numbers)  # Calculate the mean of the numbers
+print(mean_random)                   # Print the mean value
+
+
+# Another edit to trigger a push
